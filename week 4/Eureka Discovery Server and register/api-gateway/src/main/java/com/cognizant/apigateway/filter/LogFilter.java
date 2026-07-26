3a1e5b872084360295d95e2f4e2bb92f9b4dc945
Nirfan206 <203888097+Nirfan206@users.noreply.github.com>
2026-07-26 10:06:56 +0530

    Update LogFilter.java

--- week 4/Eureka Discovery Server and register/api-gateway/src/main/java/com/cognizant/apigateway/filter/LogFilter.java
+++ week 4/Eureka Discovery Server and register/api-gateway/src/main/java/com/cognizant/apigateway/filter/LogFilter.java
@@ -1,1 +1,26 @@
+package com.cognizant.apigateway.filter;
 
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LogFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(org.springframework.web.server.ServerWebExchange exchange,
+                             org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
+
+        System.out.println("Incoming Request");
+        System.out.println("Method : " + exchange.getRequest().getMethod());
+        System.out.println("URI : " + exchange.getRequest().getURI());
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
