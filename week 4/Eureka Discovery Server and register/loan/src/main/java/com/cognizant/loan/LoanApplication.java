80bbf30ae3e2ea4cdd9b770fbbcc41f141b2ce33
Nirfan206 <203888097+Nirfan206@users.noreply.github.com>
2026-07-26 09:14:50 +0530

    Update LoanApplication.java

--- week 4/Eureka Discovery Server and register/loan/src/main/java/com/cognizant/loan/LoanApplication.java
+++ week 4/Eureka Discovery Server and register/loan/src/main/java/com/cognizant/loan/LoanApplication.java
@@ -1,1 +1,15 @@
-locan appl
+package com.cognizant.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LoanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplication.class, args);
+    }
+
+}
