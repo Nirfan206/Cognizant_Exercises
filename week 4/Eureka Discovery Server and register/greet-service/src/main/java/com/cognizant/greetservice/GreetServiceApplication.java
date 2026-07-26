da21f199bbd51d6605481e6ba58d542ee0232c74
Nirfan206 <203888097+Nirfan206@users.noreply.github.com>
2026-07-26 10:04:24 +0530

    Update GreetServiceApplication.java

--- week 4/Eureka Discovery Server and register/greet-service/src/main/java/com/cognizant/greetservice/GreetServiceApplication.java
+++ week 4/Eureka Discovery Server and register/greet-service/src/main/java/com/cognizant/greetservice/GreetServiceApplication.java
@@ -1,1 +1,15 @@
-GreetServiceApplication.java
+package com.cognizant.greetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GreetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetServiceApplication.class, args);
+    }
+
+}
