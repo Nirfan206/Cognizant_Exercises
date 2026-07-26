fbc407cadface86a6e1310bbe7508a487bf7716a
Nirfan206 <203888097+Nirfan206@users.noreply.github.com>
2026-07-26 19:46:58 +0530

    Add main App component with conditional rendering

---
+++ week 5/13-ReactJS-HOL/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import BookDetails from "./BookDetails";
+import BlogDetails from "./BlogDetails";
+import CourseDetails from "./CourseDetails";
+
+function App() {
+
+  const showBooks = true;
+  const showBlogs = true;
+  const showCourses = true;
+
+  return (
+    <div style={{ padding: "20px" }}>
+
+      <h1>Blogger App</h1>
+
+      {/* Conditional Rendering using && */}
+      {showBooks && <BookDetails />}
+
+      {/* Conditional Rendering using Ternary Operator */}
+      {showBlogs ? <BlogDetails /> : <h3>No Blogs Available</h3>}
+
+      {/* Conditional Rendering using Element Variable */}
+      {(() => {
+        let component;
+        if (showCourses) {
+          component = <CourseDetails />;
+        } else {
+          component = <h3>No Courses Available</h3>;
+        }
+        return component;
+      })()}
+
+    </div>
+  );
+}
+
+export default App;
