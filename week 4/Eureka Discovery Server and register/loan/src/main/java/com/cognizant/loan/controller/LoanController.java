d67ff938d403837269f133dae77abb2d6782145b
Nirfan206 <203888097+Nirfan206@users.noreply.github.com>
2026-07-26 09:15:07 +0530

    Update LoanController.java

--- week 4/Eureka Discovery Server and register/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
+++ week 4/Eureka Discovery Server and register/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -1,2 +1,14 @@
-loancontroller
-  
+package com.cognizant.loan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loan")
+    public String getLoan() {
+        return "Welcome to Loan Service";
+    }
+
+}
